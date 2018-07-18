cf40f2c9ade97ff5c06690052894730a52225023
Avi Saban <Avi.Saban@emc.com>
2018-07-18 15:31:17 +0300

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {range} from 'rxjs';
+import {a} from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,57 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  private isXTurn: boolean;
+  boxContents: BoxContent[];
+
+  constructor() {
+    this.initBoard();
+  }
+
+  private initBoard() {
+    this.isXTurn = true;
+    this.boxContents = [];
+    for (let i = 0; i <= 8; i++) {
+      const boxContent = {
+        id: i,
+        name: '',
+      };
+      this.boxContents.push(boxContent);
+    }
+    console.log(this.boxContents);
+  }
+
+  newGame() {
+    this.initBoard();
+  }
+
+  setBox(boxContent) {
+    if (boxContent.name != '') {
+      return;
+    }
+    else {
+      if (this.isXTurn) {
+        boxContent.name = Player.X;
+        this.isXTurn = false;
+      }
+      else {
+        boxContent.name = Player.O;
+        this.isXTurn = true;
+      }
+    }
+    // if (isTheGameEnd()){
+    //
+    // }
+  }
+}
+
+interface BoxContent {
+  id: number;
+  name: string;
+}
+
+enum Player {
+  EMPTY = "",
+  X = "X",
+  O = "0",
 }
